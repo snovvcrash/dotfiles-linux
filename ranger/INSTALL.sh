ff57ac09bc521a4594387d28044bb9baa56197f2
Sam Freeside <snovvcrash@protonmail.ch>
2019-04-16 10:47:10 +0300

    Update INSTALL.sh

--- ranger/INSTALL.sh
+++ ranger/INSTALL.sh
@@ -1,6 +1,5 @@
 sudo apt install -y ranger caca-utils highlight atool w3m poppler-utils mediainfo
-
 ranger
 q
-
+ranger --copy-config=all
 ln -sv ${HOME}/.dotfiles/ranger/rc.conf ${HOME}/.config/ranger/rc.conf
