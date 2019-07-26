5729a27facb3bf22805b006c79b68d0882027a0d
Sam Fre3side <snovvcrash@protonmail.ch>
2019-07-25 21:02:53 -0400

    Update dotfiles

--- zsh/INSTALL.sh
+++ zsh/INSTALL.sh
@@ -1,2 +1,8 @@
 rm ${HOME}/.zshrc
 ln -sv ${HOME}/.dotfiles/zsh/.zshrc ${HOME}/.zshrc
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Or
+cp "$ZSH/themes/robbyrussell.zsh-theme" "$ZSH_CUSTOM/themes/robbyrussell.zsh-theme"  # and modify PROMPT with "%(4~|%-1~/…/%2~|%3~)"
