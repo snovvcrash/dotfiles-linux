21e0124be137c9f1460c02e87152b56319a6feab
Sam Free5ide <snovvcrash@protonmail.ch>
2020-03-28 22:03:35 +0300

    Minor edits

--- tilix/INSTALL.sh
+++ tilix/INSTALL.sh
@@ -1,4 +1,13 @@
 sudo apt install tilix -y
 mkdir -p "${HOME}"/.config/tilix/schemes
 
-# Themes: https://github.com/storm119/Tilix-Themes/blob/master/Themes.md
+# Themes:
+#   https://github.com/storm119/Tilix-Themes/blob/master/Themes.md
+#   https://github.com/isacikgoz/gogh-to-tilix
+
+git clone --recurse-submodules https://github.com/isacikgoz/gogh-to-tilix /tmp/isacikgoz/gogh-to-tilix
+cd /tmp/isacikgoz/gogh-to-tilix
+chmod +x install.sh
+./install.sh ~/.config/tilix/schemes
+cd -
+rm -rf /tmp/isacikgoz/gogh-to-tilix
