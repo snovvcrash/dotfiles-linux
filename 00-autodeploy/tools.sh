677169d15b03b8c8f88fdc384cff1957333fd680
Sam Free5ide <snovvcrash@protonmail.ch>
2020-04-08 08:02:17 -0400

    Minor edits

---
+++ 00-autodeploy/tools.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# apt
+
+sudo apt install timeshift -y
+sudo apt install keepassx -y
+
+# snap
+
+sudo apt install snapd -y
+sudo service snapd start
+
+sudo snap install mdless
