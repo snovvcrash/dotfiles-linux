c6de7ca62bcc0b29febdb74d77085f2f399a2026
Sam Freeside <snovvcrash@protonmail.ch>
2020-06-06 16:33:58 +0300

    Update python.sh

--- 00-autoconfig/python.sh
+++ 00-autoconfig/python.sh
@@ -7,12 +7,14 @@ colorecho() {
 	echo -e "${RED}${1}${NOCOLOR}"
 }
 
-sudo apt install python-pip -y
-sudo python -m pip install --upgrade pip
+# Python2 (interpreter + pip)
+#sudo apt install python2.7 -y && sudo ln -sv /usr/bin/python2.7 /usr/bin/python
+curl https://bootstrap.pypa.io/get-pip.py |python
 
+# Python3 (pip + venv)
 sudo apt install python3-pip -y
 sudo python3 -m pip install --upgrade pip
-
 sudo apt install python3-venv -y
+
 sudo apt install virtualenvwrapper -y
 colorecho "[*] Check if virtualenvwrapper.sh path is correct: $HOME/.dotfiles/system/.virtualenv"
