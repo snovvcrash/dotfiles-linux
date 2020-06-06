255039fcc89dbc07d363320f9fa1c3d25e74f8ab
Sam Freeside <snovvcrash@protonmail.ch>
2020-06-06 16:08:50 +0300

    Update zsh.sh

--- 00-autoconfig/zsh.sh
+++ 00-autoconfig/zsh.sh
@@ -7,7 +7,7 @@ colorecho() {
 	echo -e "${RED}${1}${NOCOLOR}"
 }
 
-apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
