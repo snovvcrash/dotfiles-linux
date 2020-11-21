a49356028e2bb09352be10165f4e35c45b42dc2a
Sam Frees1de <snovvcrash@protonmail.ch>
2020-11-21 21:25:48 +0300

    Minor edits

--- 00-autoconfig/python.sh
+++ 00-autoconfig/python.sh
@@ -4,17 +4,17 @@ RED="\033[1;31m"
 NOCOLOR="\033[0m"
 
 colorecho() {
-	echo -e "${RED}${1}${NOCOLOR}"
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
 }
-
-# Python2 (interpreter + pip)
-#sudo apt install python2.7 -y && sudo ln -sv /usr/bin/python2.7 /usr/bin/python
-curl https://bootstrap.pypa.io/get-pip.py | sudo python
 
 # Python3 (pip + venv)
 sudo apt install python3-pip -y
 sudo python3 -m pip install --upgrade pip
 sudo apt install python3-venv -y
+
+# Python2 (interpreter + pip)
+#sudo apt install python2.7 -y && sudo ln -sv /usr/bin/python2.7 /usr/bin/python
+curl https://bootstrap.pypa.io/get-pip.py | sudo python
 
 # Pipenv
 sudo python3 -m pip install pipenv
--- 00-autoconfig/zsh.sh
+++ 00-autoconfig/zsh.sh
@@ -4,7 +4,7 @@ RED="\033[1;31m"
 NOCOLOR="\033[0m"
 
 colorecho() {
-	echo -e "${RED}${1}${NOCOLOR}"
+	echo -e "[*] ${RED}${1}${NOCOLOR}"
 }
 
 ZSH="${HOME}/.oh-my-zsh"
@@ -14,8 +14,8 @@ sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
-colorecho '[*] Re-login'
-colorecho '[*] Run "sudo chsh -s `which zsh`"'
+colorecho 'Re-login'
+colorecho 'Run "sudo chsh -s `which zsh`"'
 
 cp "$ZSH/themes/robbyrussell.zsh-theme" "$ZSH_CUSTOM/themes/robbyrussell.zsh-theme"
 sed -i 's/%c/%(4~|%-1~\/…\/%2~|%3~)/g' "$ZSH_CUSTOM/themes/robbyrussell.zsh-theme"
