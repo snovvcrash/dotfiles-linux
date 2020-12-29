0e8e745505077b0630db2143c49afc7a9a615e22
Sam Free5ide <snovvcrash@protonmail.ch>
2020-12-29 16:30:06 -0500

    Minor edits

--- 00-autoconfig/tools.sh
+++ 00-autoconfig/tools.sh
@@ -14,3 +14,10 @@ sudo service snapd start
 
 sudo snap install codium
 sudo snap install mdless
+
+# Sublime Text
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update && sudo apt install sublime-text -y
