cee518bf191b62226a3b28e2dbbc4cd786a8c52c
snovvcrash <snovvcrash@protonmail.ch>
2021-03-24 21:29:37 +0300

    Update python.sh

--- 00-autoconfig/python.sh
+++ 00-autoconfig/python.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-RED="\033[1;31m"
-NOCOLOR="\033[0m"
-
-colorecho() {
-	echo -e "[*] ${RED}${1}${NOCOLOR}"
-}
-
-ZSH="${HOME}/.oh-my-zsh"
-ZSH_CUSTOM="$ZSH/custom"
-
 # Python3 (pip + venv)
 sudo apt install python3-pip -y
 sudo python3 -m pip install --upgrade pip
 sudo apt install python3-venv -y
+
+# Python 3 (latest interpreter + pip)
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.9 -y
+sudo apt install python3.9-distutils -y
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3.9
 
 # Python2 (interpreter + pip)
 #sudo apt install python2.7 -y && sudo ln -sv /usr/bin/python2.7 /usr/bin/python
