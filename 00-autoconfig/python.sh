bf2e3d7ec7dbba7d8dafd0b62ed5f22583539e33
snovvcrash <snovvcrash@protonmail.ch>
2021-04-10 21:00:25 +0300

    Update python.sh

--- 00-autoconfig/python.sh
+++ 00-autoconfig/python.sh
@@ -11,11 +11,11 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3.9 -y
 sudo apt install python3.9-distutils -y
-curl https://bootstrap.pypa.io/get-pip.py | sudo python3.9
+curl https://bootstrap.pypa.io/pip/get-pip.py | sudo python3.9
 
 # Python2 (interpreter + pip)
 #sudo apt install python2.7 -y && sudo ln -sv /usr/bin/python2.7 /usr/bin/python
-curl https://bootstrap.pypa.io/2.7/get-pip.py | sudo python
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | sudo python
 
 # Poetry
 sudo python3 -m pip install --ignore-installed poetry
