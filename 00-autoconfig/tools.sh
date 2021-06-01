882edc115155a7849512b2ebf69536cbf1985485
snovvcrash <snovvcrash@protonmail.ch>
2021-06-01 15:39:20 +0300

    Update tools.sh

--- 00-autoconfig/tools.sh
+++ 00-autoconfig/tools.sh
@@ -22,4 +22,5 @@ sudo snap install mdless
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install apt-transport-https -y
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && sudo apt install sublime-text -y
+sudo apt update
+sudo apt install sublime-text -y
