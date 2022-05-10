74877f203c716353f870210f1a2f320fccc4e0ff
snovvcrash <snovvcrash@protonmail.ch>
2022-05-10 21:12:14 +0300

    Update tools.sh

--- 00-autoconfig/tools.sh
+++ 00-autoconfig/tools.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
-# apt
+# -- apt -------------------------------------------------------------
 
+sudo apt install autossh -y
+sudo apt install bat -y
+sudo apt install clipman -y
+sudo apt install flameshot -y
+sudo apt install jq -y
+sudo apt install keepassx -y
 sudo apt install mlocate -y
+sudo apt install ncdu -y
 sudo apt install remmina -y
 sudo apt install ripgrep -y
-sudo apt install bat -y
+sudo apt install ssh-tools -y
+sudo apt install timeshift -y
 sudo apt install tree -y
 sudo apt install xfce4-clipman -y
-sudo apt install flameshot -y
-sudo apt install keepassx -y
-sudo apt install timeshift -y
-sudo apt install clipman -y
 
-# snap
+# Sublime Text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https -y
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text -y
+
+# -- snap ------------------------------------------------------------
 
 sudo apt install snapd -y
 sudo service snapd start
@@ -23,11 +34,3 @@ sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap*
 
 sudo snap install codium --classic
 sudo snap install mdless
-
-# Sublime Text
-
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt install apt-transport-https -y
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-text -y
