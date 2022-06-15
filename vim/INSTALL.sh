e400c452c9b7168010213a1adf99eebdd989c392
Sam Free5ide <snovvcrash@protonmail.ch>
2022-06-15 23:39:42 +0300

    Edit vim dotfiles

--- vim/INSTALL.sh
+++ vim/INSTALL.sh
@@ -1,2 +1,6 @@
-rm ${HOME}/.vimrc
+rm -rf ${HOME}/{.vimrc,.vim}
 ln -sv ${HOME}/.dotfiles/vim/vimrc ${HOME}/.vimrc
+
+mkdir -p ~/.vim/pack/plugins/{start,opt}
+
+git clone https://github.com/PProvost/vim-ps1.git ~/.vim/pack/plugins/start/vim-ps1
