84cd14780d4ffac9d5614038844022be6cd11758
Sam Freeside <snovvcrash@protonmail.ch>
2023-02-12 20:54:24 +0300

    Minor edits

--- 00-autoconfig/tools.sh
+++ 00-autoconfig/tools.sh
@@ -29,6 +29,7 @@ sudo apt install sublime-text -y
 # eget
 curl https://zyedidia.github.io/eget.sh | sh
 sudo mv eget /usr/local/bin
+eget -qs linux/amd64 "ipinfo/cli" --to /tmp && sudo dpkg -i /tmp/ipinfo_*linux_amd64 && rm /tmp/ipinfo_*linux_amd64
 
 # -- snap ------------------------------------------------------------
 
