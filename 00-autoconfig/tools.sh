49bc0aed1e20761f402a5d99f53784b42d8c317a
Sam Freeside <snovvcrash@protonmail.ch>
2023-02-18 18:45:27 +0300

    Minor edits

--- 00-autoconfig/tools.sh
+++ 00-autoconfig/tools.sh
@@ -18,6 +18,7 @@ sudo apt install ssh-tools -y
 sudo apt install timeshift -y
 sudo apt install tree -y
 sudo apt install xfce4-clipman -y
+sudo apt install bless -y
 
 # Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
