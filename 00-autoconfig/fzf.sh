cab54b8ee229857acd6aa9f311482bf5c3b49b5e
Sam Freeside <snovvcrash@protonmail.ch>
2025-05-01 21:15:14 +0300

    Minor edits

--- 00-autoconfig/fzf.sh
+++ 00-autoconfig/fzf.sh
@@ -4,3 +4,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
 ${HOME}/.fzf/install
 
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+build-fzf-tab-module
+
+sed -i 's/cursor_y \* 2 > window_height/cursor_y \* 1\.8 > window_height/g' ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab/lib/ftb-tmux-popup
